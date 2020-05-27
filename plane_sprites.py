2c5ba76a66ab4ecb17b2695e797e6051c4aadff9
Shen <sqshen@ebay.com>
2020-05-27 22:04:17 +0800

    sq

---
+++ plane_sprites.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class GameSprite(pygame.sprite.Sprite):
+    """飞机大战游戏精灵"""
+    def __init__(self, image_name, speed=1):
+
+        # 调用父类初始化方法
+        super().__init__()
+
+        # 定义对象属性
+        self.image = pygame.image.load(image_name)
+        self.rect = self.image.get_rect()
+        self.speed = speed
+
+    def update(self):
+        # 精灵在屏幕垂直方向移动
+        self.rect.y += self.speed
+
+
+
+
+
--- pygame01.py
+++ pygame01.py
@@ -1,16 +1,42 @@
 import pygame
+from plane_sprites import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((480, 700))
-
 bg = pygame.image.load("./images/background.png")
 hero = pygame.image.load("./images/me1.png")
 screen.blit(bg, (0, 0))
 screen.blit(hero, (185, 500))
 pygame.display.update()
 
+clock = pygame.time.Clock()
+hero_rec = pygame.Rect(185, 500, 102, 126)
+
+# 创建敌机精灵
+enemy = GameSprite("./images/enemy1.png")
+# 创建敌机精灵组
+enemy_group = pygame.sprite.Group(enemy)
+
 while True:
+    clock.tick(60)
+
+    event_list = pygame.event.get()
+
+    if hero_rec.y < -126:
+        hero_rec.y = 700
+    # elif hero_rec.y > 600:
+    #     hero_rec.y = 600
+    hero_rec.y -= 2
+    screen.blit(bg, (0, 0))
+    screen.blit(hero, hero_rec)
+
+    # 精灵组调用update 和 draw
+    enemy_group.update()
+    enemy_group.draw(screen)
+
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("退出游戏...")
